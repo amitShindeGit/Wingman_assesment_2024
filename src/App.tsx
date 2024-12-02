b6ae73231e02c093da3cdaab8bebb9bc21973fe7
amitShindeGit <99amitshinde@gmail.com>
2024-12-02 09:51:56 +0530

    Added theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import ThemeButton from "./components/ThemeButton";
 
 function App() {
   return (
-    <div className="bg-stone-200">
+    <div className="bg-stone-200 dark:bg-gray-500">
+      <ThemeButton />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="*" element={<h1>Not found </h1>} />
